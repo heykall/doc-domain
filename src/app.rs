dc1807d13bdaed4556519b2a08e37eec489b249f
heykall <[muh.heykall@gmail.com]>
2022-03-18 04:06:04 +0700

    register and login section done

--- src/app.rs
+++ src/app.rs
@@ -334,13 +334,14 @@ pub fn app() -> Html {
             </ul>
         </div>
     </div>
-            <div class="col-span-4 divide-y-4 divide-gray-700">
+            <div class="col-span-4 divide-y-4 divide-slate-400/25">
+            // Introduction section
               <div class="grid grid-cols-2">
                 <div class="relative items-center w-full mx-auto">
                   <div class="grid grid-cols-1 ">
-                    <div class="w-full h-max max-w-lg mx-auto my-4 bg-white shadow-xl rounded-xl">
-                      <div class="p-6">
-                        <h4 class="mt-8 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl ">{"Introduction"}</h4>
+                    <div class="w-full h-max max-w-lg mx-auto my-4 bg-white">
+                      <div class="p-4">
+                        <h4 class="my-2 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl ">{"Introduction"}</h4>
                         <p class="mt-3 text-base leading-relaxed text-gray-500">
                         <span class="mt-3 text-base leading-relaxed text-gray-500">{"This document describes the Domain Management API, which is meant to be used by back-end servers or trusted parties performing administrative tasks. Generally speaking, anything that can be done through the Domain dashboard (and more) can also be done through this API.
                         This API is separate from the publicly accessible Auth0 Authentication API, which is meant to be used by front-ends and untrusted parties.
@@ -354,17 +355,223 @@ pub fn app() -> Html {
               </div>
 
                     
-                    <div class="relative items-center w-full mx-auto bg-gray-500">
-                      
-                    </div>
+              <div class="relative items-center w-full mx-auto bg-gray-500">
+                
               </div>
 
+              </div>
+
+              // Register Section
               <div class="grid grid-cols-2">
                   <div class="relative items-center w-full mx-auto">
                     <div class="grid grid-cols-1 ">
-                    <div class="w-full h-max max-w-lg mx-auto my-4 bg-white shadow-xl rounded-xl">
-                        <div class="p-6">
-                          <h4 class="mt-8 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl ">{"Get clients"}</h4>
+                      <div class="w-full h-max max-w-lg mx-auto my-4 bg-white">
+                        <div class="p-4">
+                          <h4 class="text-2xl my-2 font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl ">{"Create Account"}</h4>
+                          <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-semibold leading-none text-white bg-green-400 rounded-full">{"POST"}</span>
+                          <span class="mt-3 text-base leading-relaxed text-gray-500">{"/register"}</span>
+                          <p class="mt-3 text-base leading-relaxed text-gray-500">
+                          <span class="mt-3 text-base leading-relaxed text-gray-500">{"Given a user's credentials, and a "}</span>
+                          <span class="mt-3 text-base leading-relaxed text-gray-500 bg-gray-300 javascript">{"connection"}</span>
+                          <span class="mt-3 text-base leading-relaxed text-gray-500">{", this endpoint will create a new user using active authentication. This endpoint only works for database connections."}</span>
+                          </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    
+                    <div class="relative items-center w-full mx-auto bg-gray-500">
+                    // Request body section
+                      <div>
+                        <div class="p-4">
+                          <h4 class="my-2 text-lg p-5 font-medium leading-none tracking-tighter text-white lg:text-lg ">{"Request Body Example"}</h4>
+                          <h4 class="text-base font-medium leading-none text-yellow-500 tracking-tighter text-white lg:text-base ">{"Request Body Type : application/x-www-form-urlencoded "}</h4>
+                        </div>
+                        <pre>
+                          <code class="json text-xs">
+                            {r#"
+                            {
+                              "email": "blabla@email.com",
+                              "password": "password minimal 8, huruf kecil, huruf besar, nomer",
+                              "name": "username",
+                              "account_type": "personal / businness",
+                              "region": "us / au / jp / eu",
+                              "tenant_name": "first line of domain for users",
+                            }                       
+                            "#}
+                            </code>
+                        </pre>        
+                      </div>
+                      
+                      // Req Headers Section
+                      <div>
+                        <div class="p-4">
+                          <h4 class="my-2 text-lg p-5 font-medium leading-none tracking-tighter text-white lg:text-lg ">{"Request Headers"}</h4>
+                        </div>
+                        <pre>
+                          <code class="json text-lg">
+                            {r#"
+
+
+
+                            No Headers Required
+                            
+                            
+
+                            "#}
+                            </code>
+                        </pre>        
+                      </div>
+                      
+                      // Response section
+                      <div>
+                        <div class="p-4">
+                          <h4 class="my-2 text-lg p-5 font-medium leading-none tracking-tighter text-white lg:text-lg ">{"Response Example"}</h4>
+                        </div>
+                        <pre>
+                          <code class="json text-xs">
+                            {r#"
+
+                            Status Code 201 - Created
+
+                            {
+                              "message": "You are sucessfully registered.",
+                              "data": "registered email"
+                            }
+
+                            Status Code 400 - Bad Request(validate field email dan password)
+                            
+                            {
+                              "message": "validation error",
+                              "data": ""
+                            }
+
+                            Status Code 400 - Bad Request(validate unique email)
+                            
+                            {
+                                "message": "Email is already used.",
+                                "data": ""
+                            }
+
+                            Status Code 500 - Internal Server Error
+                            
+                            {
+                              "message": "Internal Server Error",
+                              "data": ""
+                            }
+
+                            "#}
+                            </code>
+                        </pre>        
+                      </div>
+
+                    </div>
+              </div>
+
+              // Login Section
+              <div class="grid grid-cols-2">
+                  <div class="relative items-center w-full mx-auto">
+                    <div class="grid grid-cols-1 ">
+                      <div class="w-full h-max max-w-lg mx-auto my-4 bg-white">
+                        <div class="p-4">
+                          <h4 class="text-2xl my-2 font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl ">{"Login Account"}</h4>
+                          <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-semibold leading-none text-white bg-green-400 rounded-full">{"POST"}</span>
+                          <span class="mt-3 text-base leading-relaxed text-gray-500">{"/login"}</span>
+                          <p class="mt-3 text-base leading-relaxed text-gray-500">
+                          <span class="mt-3 text-base leading-relaxed text-gray-500">{"Given the social provider's Access Token and the connection, this endpoint will authenticate the user with the provider and return a JSON with the Access Token."}</span>
+                          </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    
+                    <div class="relative items-center w-full mx-auto bg-gray-500">
+                    // Request body section
+                      <div>
+                        <div class="p-4">
+                          <h4 class="my-2 text-lg p-5 font-medium leading-none tracking-tighter text-white lg:text-lg ">{"Request Body Example"}</h4>
+                          <h4 class="text-base font-medium leading-none text-yellow-500 tracking-tighter text-white lg:text-base ">{"Request Body Type : application/x-www-form-urlencoded "}</h4>
+                        </div>
+                        <pre>
+                          <code class="json text-xs">
+                            {r#"
+                            {
+                              "email": "blabla@email.com",
+                              "password": "password minimal 8, huruf kecil, huruf besar, nomer"
+                            }             
+                            "#}
+                            </code>
+                        </pre>        
+                      </div>
+                      
+                      // Req Headers Section
+                      <div>
+                        <div class="p-4">
+                          <h4 class="my-2 text-lg p-5 font-medium leading-none tracking-tighter text-white lg:text-lg ">{"Request Headers"}</h4>
+                        </div>
+                        <pre>
+                          <code class="json text-lg">
+                            {r#"
+
+
+
+                            No Headers Required
+                            
+                            
+
+                            "#}
+                            </code>
+                        </pre>        
+                      </div>
+                      
+                      // Response section
+                      <div>
+                        <div class="p-4">
+                          <h4 class="my-2 text-lg p-5 font-medium leading-none tracking-tighter text-white lg:text-lg ">{"Response Example"}</h4>
+                        </div>
+                        <pre>
+                          <code class="json text-xs">
+                            {r#"
+
+                            Status Code 200 - Ok
+
+                            {
+                              "email": "blabla@email.com",
+                              "username": "username",
+                              "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2MjBlMGZmMzk5YzgwMDhkZjY4Yzc0OTIiLCJleHAiOjE2NDUxNzUxNTUsImlhdCI6MTY0NTA4ODc1NSwiZW1haWwiOiJoZXlrYWxsQGdtYWlsLmNvbSIsInRlbmFudCI6IkRvbWFpbiIsInNjb3BlcyI6InJlYWQ6Y2xpZW50X2dyYW50cyBjcmVhdGU6Y2xpZW50X2dyYW50cyBkZWxldGU6Y2xpZW50X2dyYW50cyB1cGRhdGU6Y2xpZW50X2dyYW50cyByZWFkOnVzZXJzIHVwZGF0ZTp1c2VycyBkZWxldGU6dXNlcnMgY3JlYXRlOnVzZXJzIHJlYWQ6dXNlcnNfYXBwX21ldGFkYXRhIHVwZGF0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgZGVsZXRlOnVzZXJzX2FwcF9tZXRhZGF0YSBjcmVhdGU6dXNlcnNfYXBwX21ldGFkYXRhIHJlYWQ6dXNlcl9jdXN0b21fYmxvY2tzIGNyZWF0ZTp1c2VyX2N1c3RvbV9ibG9ja3MgZGVsZXRlOnVzZXJfY3VzdG9tX2Jsb2NrcyBjcmVhdGU6dXNlcl90aWNrZXRzIHJlYWQ6Y2xpZW50cyB1cGRhdGU6Y2xpZW50cyBkZWxldGU6Y2xpZW50cyBjcmVhdGU6Y2xpZW50cyByZWFkOmNsaWVudF9rZXlzIHVwZGF0ZTpjbGllbnRfa2V5cyBkZWxldGU6Y2xpZW50X2tleXMgY3JlYXRlOmNsaWVudF9rZXlzIHJlYWQ6Y29ubmVjdGlvbnMgdXBkYXRlOmNvbm5lY3Rpb25zIGRlbGV0ZTpjb25uZWN0aW9ucyBjcmVhdGU6Y29ubmVjdGlvbnMgcmVhZDpyZXNvdXJjZV9zZXJ2ZXJzIHVwZGF0ZTpyZXNvdXJjZV9zZXJ2ZXJzIGRlbGV0ZTpyZXNvdXJjZV9zZXJ2ZXJzIGNyZWF0ZTpyZXNvdXJjZV9zZXJ2ZXJzIHJlYWQ6ZGV2aWNlX2NyZWRlbnRpYWxzIHVwZGF0ZTpkZXZpY2VfY3JlZGVudGlhbHMgZGVsZXRlOmRldmljZV9jcmVkZW50aWFscyBjcmVhdGU6ZGV2aWNlX2NyZWRlbnRpYWxzIHJlYWQ6cnVsZXMgdXBkYXRlOnJ1bGVzIGRlbGV0ZTpydWxlcyBjcmVhdGU6cnVsZXMgcmVhZDpydWxlc19jb25maWdzIHVwZGF0ZTpydWxlc19jb25maWdzIGRlbGV0ZTpydWxlc19jb25maWdzIHJlYWQ6aG9va3MgdXBkYXRlOmhvb2tzIGRlbGV0ZTpob29rcyBjcmVhdGU6aG9va3MgcmVhZDphY3Rpb25zIHVwZGF0ZTphY3Rpb25zIGRlbGV0ZTphY3Rpb25zIGNyZWF0ZTphY3Rpb25zIHJlYWQ6ZW1haWxfcHJvdmlkZXIgdXBkYXRlOmVtYWlsX3Byb3ZpZGVyIGRlbGV0ZTplbWFpbF9wcm92aWRlciBjcmVhdGU6ZW1haWxfcHJvdmlkZXIgYmxhY2tsaXN0OnRva2VucyByZWFkOnN0YXRzIHJlYWQ6aW5zaWdodHMgcmVhZDp0ZW5hbnRfc2V0dGluZ3MgdXBkYXRlOnRlbmFudF9zZXR0aW5ncyByZWFkOmxvZ3MgcmVhZDpsb2dzX3VzZXJzIHJlYWQ6c2hpZWxkcyBjcmVhdGU6c2hpZWxkcyB1cGRhdGU6c2hpZWxkcyBkZWxldGU6c2hpZWxkcyByZWFkOmFub21hbHlfYmxvY2tzIGRlbGV0ZTphbm9tYWx5X2Jsb2NrcyB1cGRhdGU6dHJpZ2dlcnMgcmVhZDp0cmlnZ2VycyByZWFkOmdyYW50cyBkZWxldGU6Z3JhbnRzIHJlYWQ6Z3VhcmRpYW5fZmFjdG9ycyB1cGRhdGU6Z3VhcmRpYW5fZmFjdG9ycyByZWFkOmd1YXJkaWFuX2Vucm9sbG1lbnRzIGRlbGV0ZTpndWFyZGlhbl9lbnJvbGxtZW50cyBjcmVhdGU6Z3VhcmRpYW5fZW5yb2xsbWVudF90aWNrZXRzIHJlYWQ6dXNlcl9pZHBfdG9rZW5zIGNyZWF0ZTpwYXNzd29yZHNfY2hlY2tpbmdfam9iIGRlbGV0ZTpwYXNzd29yZHNfY2hlY2tpbmdfam9iIHJlYWQ6Y3VzdG9tX2RvbWFpbnMgZGVsZXRlOmN1c3RvbV9kb21haW5zIGNyZWF0ZTpjdXN0b21fZG9tYWlucyB1cGRhdGU6Y3VzdG9tX2RvbWFpbnMgcmVhZDplbWFpbF90ZW1wbGF0ZXMgY3JlYXRlOmVtYWlsX3RlbXBsYXRlcyB1cGRhdGU6ZW1haWxfdGVtcGxhdGVzIHJlYWQ6bWZhX3BvbGljaWVzIHVwZGF0ZTptZmFfcG9saWNpZXMgcmVhZDpyb2xlcyBjcmVhdGU6cm9sZXMgZGVsZXRlOnJvbGVzIHVwZGF0ZTpyb2xlcyByZWFkOnByb21wdHMgdXBkYXRlOnByb21wdHMgcmVhZDpicmFuZGluZyB1cGRhdGU6YnJhbmRpbmcgZGVsZXRlOmJyYW5kaW5nIHJlYWQ6bG9nX3N0cmVhbXMgY3JlYXRlOmxvZ19zdHJlYW1zIGRlbGV0ZTpsb2dfc3RyZWFtcyB1cGRhdGU6bG9nX3N0cmVhbXMgY3JlYXRlOnNpZ25pbmdfa2V5cyByZWFkOnNpZ25pbmdfa2V5cyB1cGRhdGU6c2lnbmluZ19rZXlzIHJlYWQ6bGltaXRzIHVwZGF0ZTpsaW1pdHMgY3JlYXRlOnJvbGVfbWVtYmVycyByZWFkOnJvbGVfbWVtYmVycyBkZWxldGU6cm9sZV9tZW1iZXJzIHJlYWQ6ZW50aXRsZW1lbnRzIHJlYWQ6YXR0YWNrX3Byb3RlY3Rpb24gdXBkYXRlOmF0dGFja19wcm90ZWN0aW9uIHJlYWQ6b3JnYW5pemF0aW9ucyB1cGRhdGU6b3JnYW5pemF0aW9ucyBjcmVhdGU6b3JnYW5pemF0aW9ucyBkZWxldGU6b3JnYW5pemF0aW9ucyBjcmVhdGU6b3JnYW5pemF0aW9uX21lbWJlcnMgcmVhZDpvcmdhbml6YXRpb25fbWVtYmVycyBkZWxldGU6b3JnYW5pemF0aW9uX21lbWJlcnMgY3JlYXRlOm9yZ2FuaXphdGlvbl9jb25uZWN0aW9ucyByZWFkOm9yZ2FuaXphdGlvbl9jb25uZWN0aW9ucyB1cGRhdGU6b3JnYW5pemF0aW9uX2Nvbm5lY3Rpb25zIGRlbGV0ZTpvcmdhbml6YXRpb25fY29ubmVjdGlvbnMgY3JlYXRlOm9yZ2FuaXphdGlvbl9tZW1iZXJfcm9sZXMgcmVhZDpvcmdhbml6YXRpb25fbWVtYmVyX3JvbGVzIGRlbGV0ZTpvcmdhbml6YXRpb25fbWVtYmVyX3JvbGVzIGNyZWF0ZTpvcmdhbml6YXRpb25faW52aXRhdGlvbnMgcmVhZDpvcmdhbml6YXRpb25faW52aXRhdGlvbnMgZGVsZXRlOm9yZ2FuaXphdGlvbl9pbnZpdGF0aW9ucyJ9.P0IhQqmzWe5uQ1eQxQTIX2xESNaGvcCeN1qwJs3247g"
+                            }
+
+                            Status Code 401 - Unauthorized
+                            
+                            {
+                              "message": "Wrong username or password, please try again",
+                              "data": ""
+                            }
+                            
+                            Status Code 500 - Internal Server Error
+                            
+                            {
+                              "message": "Internal Server Error",
+                              "data": ""
+                            }
+
+                            "#}
+                            </code>
+                        </pre>        
+                      </div>
+
+                    </div>
+              </div>
+              
+              // Get Client Section
+              <div class="grid grid-cols-2">
+                  <div class="relative items-center w-full mx-auto">
+                    <div class="grid grid-cols-1 ">
+                    <div class="w-full h-max max-w-lg mx-auto my-4 bg-white">
+                        <div class="p-4">
+                          <h4 class="text-2xl my-2 font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl ">{"Get clients"}</h4>
                           <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-semibold leading-none text-white bg-violet-400 rounded-full">{"GET"}</span>
                           <span class="mt-3 text-base leading-relaxed text-gray-500">{"/api/v2/clients"}</span>
                           <p class="mt-3 text-base leading-relaxed text-gray-500">
@@ -383,7 +590,9 @@ pub fn app() -> Html {
 
                     
                     <div class="relative items-center w-full mx-auto bg-gray-500">
-                    <h4 class="mt-8 text-lg p-5 font-medium leading-none tracking-tighter text-white lg:text-lg ">{"Response Example"}</h4>
+                    <div class="p-4">
+                    <h4 class="my-2 text-lg p-5 font-medium leading-none tracking-tighter text-white lg:text-lg ">{"Response Example"}</h4>
+                    </div>
                       <pre>
                         <code class="json text-xs">
                           {r#"
@@ -474,57 +683,3 @@ pub fn app() -> Html {
  
     }
 }
-
-// pub struct Basic {}
-
-// pub enum Msg {}
-
-// impl Component for Basic {
-//     type Message = Msg;
-//     type Properties = ();
-
-//     fn create(_ctx: &Context<Self>) -> Self {
-//         Basic {}
-//     }
-
-//     fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-//         true
-//     }
-
-//     fn rendered(&mut self, _ctx: &Context<Self>, first_render: bool) {
-//         if first_render {
-//             // ConsoleService::info("This is first render in app tab settings");
-//             // self.link.send_message(Msg::RequestAppList);
-//             highlight();
-//         }
-//     }
-
-//     fn changed(&mut self, _ctx: &Context<Self>) -> bool {
-//         false
-//     }
-
-//     fn view(&self, _ctx: &Context<Self>) -> Html {
-//         html! {
-//             <>
-//             <pre><code class="json">{r#"{
-//                           "FirstName": "John",
-//                           "LastName": "Doe",
-//                           "Age": 43,
-//                           "Address": {
-//                               "Street": "Downing Street 10",
-//                               "City": "London",
-//                               "Country": "Great Britain"
-//                           },
-//                           "PhoneNumbers": [
-//                               "+44 1234567",
-//                               "+44 2345678"
-//                           ]
-//                       }"#
-//                        }</code></pre>
-                
-//                        <pre><code class="arcade">{"Hello World"
-//                      }</code></pre>
-//             </>
-//         }
-//     }
-// }
